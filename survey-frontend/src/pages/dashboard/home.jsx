6fe640970cfe8e229d4c3c2ad3fe8cf1694359f1
Onur BiLKE <64010601+bilkeonur@users.noreply.github.com>
2024-02-19 08:21:33 +0300

    fix: vite config file fixes

---
+++ survey-frontend/src/pages/dashboard/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Home() {
+  return (
+    <div>
+      Onur
+    </div>
+  );
+}
+
+export default Home;
---
+++ survey-frontend/src/routes.jsx
@@ -0,0 +1,23 @@
+import { HomeIcon } from "@heroicons/react/24/solid";
+import { Home } from "@/pages/dashboard";
+  
+const icon = {
+    className: "w-5 h-5 text-inherit",
+};
+
+export const routes = [
+    {
+        layout: "dashboard",
+        pages: [
+            {
+                icon: <HomeIcon {...icon} />,
+                name: "dashboard",
+                path: "/home",
+                element: <Home />
+            }
+        ]
+    }
+];
+
+export default routes;
+  
